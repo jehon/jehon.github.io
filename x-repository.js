2f4d29d62c752f23b88f7a52285863fa9d115a0a
Jean Honlet <jehon@users.noreply.github.com>
2021-11-15 21:49:53 +0100

    feat: add secondary infos

--- x-repository.js
+++ x-repository.js
@@ -4,13 +4,19 @@ class XRepository extends HTMLElement {
         return "x-repository";
     }
 
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' })
+    }
+
     connectedCallback() {
         const prj = this.getAttribute('prj');
-        const withPages = this.hasAttribute('pages');
 
-        this.innerHTML = `
-            <style>
-                ${this.constructor.tag} {
+        this.shadowRoot.innerHTML = `
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                <style>
+                :host(*) {
                     display: flex;
                     flex-direction: row;
                     flex-grow: 1;
@@ -18,32 +24,44 @@ class XRepository extends HTMLElement {
                     width: 100%;
                 }
 
-                ${this.constructor.tag} > * {
+                :host(*) > * {
                     width: 100%;
                     margin: 20px;
                 }
 
-                ${this.constructor.tag} div.card-text {
+                div.card-text {
                     margin: 20px;
                     display: flex;
                     align-items: center;
                 }
 
-                ${this.constructor.tag} div.card-text > * {
+                div.card-text > * {
                     padding-right: 10px;
                 }
 
-                ${this.constructor.tag} div.card-text > #badge {
+                div.card-text > #badge {
                     width: 200px;
                     text-align: center;
                 }
-
-                ${this.constructor.tag} div.card-text > #actions {
+                div.card-text > #spacer {
                     flex-grow: 1000;
+                }
+
+                div.card-text > #inner {
+                    display: flex;
+                    flex-flow: column nowrap;
+                    text-align: center;
+                }
+
+                ::slotted(*) {
+                    margin-bottom: 5px;
+                }
+
+                div.card-text > #actions {
                     text-align: right;
                 }
 
-                ${this.constructor.tag} img {
+                img {
                     height: 25px;
                 }
                 
@@ -59,22 +77,23 @@ class XRepository extends HTMLElement {
                         </div>
                         <div id='pr'></div>
                         <div id='branches'></div>
+                        <div id='spacer'></div>
+                        <div id='inner' class='.btn-group-vertical'>
+                            <slot></slot>
+                        </div>
                         <div id='actions'>
                             <a href="https://github.com/jehon/${prj}/pulls" class="btn btn-primary">Pull requests</a>
                             <a href="https://github.com/jehon/${prj}/actions/workflows/test.yml" class="btn btn-primary">Actions</a>
-                            ${withPages
-                ? `<a href="https://jehon.github.io/${prj}" class="btn btn-secondary">Page</a>`
-                : ''}
                         </div>
                     </div>
                 </div>
             </div >
     `;
 
-        const prEl = this.querySelector('#pr');
+        const prEl = this.shadowRoot.querySelector('#pr');
         prEl.innerHTML = '';
 
-        const branchesEl = this.querySelector('#branches');
+        const branchesEl = this.shadowRoot.querySelector('#branches');
         branchesEl.innerHTML = '';
 
         const rootAPI = `https://api.github.com/repos/jehon/${prj}`;
@@ -100,6 +119,13 @@ class XRepository extends HTMLElement {
                         branchesEl.innerHTML += `<div>${br}</div>`
                     }));
             })
+
+        Array.from(this.querySelectorAll('[port]')).map(
+            el => {
+                const port = el.getAttribute('port');
+                console.log(el, port);
+            }
+        )
     }
 }
 
