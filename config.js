02920cda8d8faa44f756e44c05daeec1f348f0e8
Jean Honlet <jehon@users.noreply.github.com>
2022-10-06 21:57:26 +0000

    feat(config): add config from jehon/config

---
+++ config.js
@@ -0,0 +1,25 @@
+
+const config = await fetch("https://jehon.github.io/config/dev-config.json")
+    .then(response => response.json())
+
+export default config;
+
+// Thanks to https://stackoverflow.com/a/56253298/1954789
+function flattenObj(obj, parent, res = {}) {
+    for (let key in obj) {
+        let propName = parent ? parent + '.' + key : key;
+        if (typeof obj[key] == 'object') {
+            flattenObj(obj[key], propName, res);
+        } else {
+            res[propName] = obj[key];
+        }
+    }
+    return res;
+}
+
+export const flattenConfig = flattenObj(config);
+
+export const inject = (string) => string.replace(
+    /\{\{([0-9a-zA-Z_.]+)\}\}/g,
+    (full, name) => flattenConfig[name] ?? ""
+)
