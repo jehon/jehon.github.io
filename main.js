3c856e9523a113e35d2f5ad8ea4caea4ee96546f
Jean Honlet <jehon@users.noreply.github.com>
2023-01-20 10:34:55 +0000

    feat: refresh by click

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 import "./x-repository.js";
 import "./x-img.js";
 
+const timestampEl = document.querySelector('#timestamp');
+
 let refreshCron = 0;
 export async function refresh() {
     if (refreshCron) {
@@ -9,11 +11,12 @@ export async function refresh() {
     }
     refreshCron = setTimeout(() => refresh(), 5 * 60 * 1000);
 
-    document.querySelector('#timestamp').innerHTML = (new Date()).toTimeString().split(' ')[0];
+    timestampEl.innerHTML = (new Date()).toTimeString().split(' ')[0];
 
     return Promise.all(Array.from(document.querySelectorAll('x-repository')).map(el => el.refreshData()));
 }
 
 document.refresh = refresh;
-
 refresh();
+
+timestampEl.addEventListener('click', () => refresh());
