b711a35e1f793e968e5d20fc10127596f6fad541
Jean Honlet <jehon@users.noreply.github.com>
2023-01-20 09:00:36 +0000

    fix: implement cron on index.html

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 import "./x-repository.js";
 import "./x-img.js";
 
+let refreshCron = 0;
+export async function refresh() {
+    if (refreshCron) {
+        clearTimeout(refreshCron)
+    }
+    refreshCron = setTimeout(() => refresh(), 5 * 60 * 1000);
+
+    return Promise.all(Array.from(document.querySelectorAll('x-repository')).map(el => el.refreshData()));
+}
+
+document.refresh = refresh;
+
+refresh();
