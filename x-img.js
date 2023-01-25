3e164018d8b1199bfd8e5a1308bc6b5282863355
Jean Honlet <jehon@users.noreply.github.com>
2023-01-25 11:23:37 +0100

    refactor: styles of x-img

--- x-img.js
+++ x-img.js
@@ -15,23 +15,37 @@ class XImg extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <style>
                 :host() {
-                    display: block;
                     height: 100%;
+                    display: block;
                 }
 
                 a {
                     height: 100%;
+                    width: 100%;
                     display: flex;
                     flex-direction: column;
+                    justify-content: flex-start;
                 }
 
                 a > img {
+                    display: block;
+                    width: 100%;
                     flex-grow: 1;
+                    flex-shrink: 1;
+                    flex-basis: 10px;
+
+                    max-height: 100%;
+                    object-fit: contain;
                 }
 
                 a > legend {
+                    display: block;
                     width: 100%;
+                    flex-grow: 0;
+                    flex-shrink: 0;
+
                     text-align: center;
+                    font-size: 10px;
                 }
             </style>
             <a href='${link}'>
