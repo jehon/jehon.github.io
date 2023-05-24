614cad523c7b77472c5f15e78973034e000c3e78
Jehon <1582670+jehon@users.noreply.github.com>
2023-05-24 12:21:09 +0000

    fix: ts was not on the correct place

--- x-repository.js
+++ x-repository.js
@@ -1,5 +1,4 @@
-
-import { octokit } from './x-github-auth.js'
+import { octokit } from "./x-github-auth.js";
 
 //
 // https://docs.github.com/en/rest/reference/repos#statuses
@@ -14,34 +13,34 @@ import { octokit } from './x-github-auth.js'
 //
 
 class XRepository extends HTMLElement {
-    static get tag() {
-        return "x-repository";
-    }
+  static get tag() {
+    return "x-repository";
+  }
 
-    /** @type {string} the repository*/
-    owner;
+  /** @type {string} the repository*/
+  owner;
 
-    /** @type {string} the repository*/
-    prj;
+  /** @type {string} the repository*/
+  prj;
 
-    /** @type {number} the level of warning */
-    warningLevel = 0;
+  /** @type {number} the level of warning */
+  warningLevel = 0;
 
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    lightWarning(level = 1, _txt = 'warning') {
-        this.warningLevel = Math.max(this.warningLevel, level);
-        this.setAttribute('warning', level);
-    }
+  lightWarning(level = 1, _txt = "warning") {
+    this.warningLevel = Math.max(this.warningLevel, level);
+    this.setAttribute("warning", level);
+  }
 
-    connectedCallback() {
-        this.prj = this.getAttribute('prj');
-        this.owner = "jehon";
+  connectedCallback() {
+    this.prj = this.getAttribute("prj");
+    this.owner = "jehon";
 
-        this.shadowRoot.innerHTML = `
+    this.shadowRoot.innerHTML = `
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                 <style>
@@ -156,197 +155,250 @@ class XRepository extends HTMLElement {
     `;
 
     this.el = Object.freeze({
-        githubBadge: this.shadowRoot.querySelector('#github-badge'),
-        version: this.shadowRoot.querySelector('#version'),
-        actions: this.shadowRoot.querySelector('#badge'),
-        pr: this.shadowRoot.querySelector('#pr'),
-        branches: this.shadowRoot.querySelector('#branches'),
-        codespaces: this.shadowRoot.querySelector('#codespaces'),
-        pages: this.shadowRoot.querySelector('#pages')
+      githubBadge: this.shadowRoot.querySelector("#github-badge"),
+      version: this.shadowRoot.querySelector("#version"),
+      actions: this.shadowRoot.querySelector("#badge"),
+      pr: this.shadowRoot.querySelector("#pr"),
+      branches: this.shadowRoot.querySelector("#branches"),
+      codespaces: this.shadowRoot.querySelector("#codespaces"),
+      pages: this.shadowRoot.querySelector("#pages"),
     });
 
-        // Problem: CORS
+    // Problem: CORS
 
-        // Array.from(this.querySelectorAll('[watch]')).map(
-        //     el => {
-        //         const url = el.getAttribute('href');
-        //         console.log(el, url);
+    // Array.from(this.querySelectorAll('[watch]')).map(
+    //     el => {
+    //         const url = el.getAttribute('href');
+    //         console.log(el, url);
 
-        //         const controller = new AbortController()
-        //         const timeoutId = setTimeout(() => {
-        //             controller.abort();
-        //             console.log("Failure: ", url);
-        //         }, 5000)
-        //         fetch(url, { signal: controller.signal }).then(response => {
-        //             console.log("Success: ", url);
-        //             clearTimeout(timeoutId);
-        //         })
-        //     }
-        // )
+    //         const controller = new AbortController()
+    //         const timeoutId = setTimeout(() => {
+    //             controller.abort();
+    //             console.log("Failure: ", url);
+    //         }, 5000)
+    //         fetch(url, { signal: controller.signal }).then(response => {
+    //             console.log("Success: ", url);
+    //             clearTimeout(timeoutId);
+    //         })
+    //     }
+    // )
 
-        this.refreshData()
-    }
+    this.refreshData();
+  }
 
-    async getWorkflowStatuses(n, branch, path = '') {
-        // https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
-        return octokit.request("GET /repos/{owner}/{repo}/actions/runs?branch={branch}&per_page={per_page}", {
+  async getWorkflowStatuses(n, branch, path = "") {
+    // https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
+    return (
+      octokit
+        .request(
+          "GET /repos/{owner}/{repo}/actions/runs?branch={branch}&per_page={per_page}",
+          {
             owner: this.owner,
             repo: this.prj,
             branch: branch,
-            per_page: n
+            per_page: n,
+          }
+        )
+        .then((result) => result.data.workflow_runs)
+        .then((runs) => (path ? runs.filter((run) => run.path == path) : runs))
+        .then((runs) => runs.map((run) => run.conclusion))
+        .then((results) => results.slice(0, 5))
+        // success, canceled, failure
+        .then((results) => {
+          if (
+            results.reduceRight(
+              (prev, current) =>
+                current == "canceled" ? prev : current == "failure",
+              false
+            )
+          )
+            this.lightWarning(branch == "main" ? 3 : 2, "runs");
+          return results;
         })
-            .then(result => result.data.workflow_runs)
-            .then(runs => path ? runs.filter(run => run.path == path) : runs)
-            .then(runs => runs.map(run => run.conclusion))
-            .then(results => results.slice(0, 5))
-            // success, canceled, failure
-            .then(results => {
-                if (results.reduceRight((prev, current) => current == 'canceled' ? prev : current == 'failure', false))
-                    this.lightWarning((branch == 'main') ? 3 : 2, 'runs'); return results;
-            })
-            .then(results => results.map(result => {
-                let char = '';
-                switch (result) {
-                    case 'success': char = '✅'; break;
-                    case 'canceled': char = '?'; break;
-                    case 'failure': char = '❌'; break;
-                    default: char = result; break;
-                }
-                return char;
-            }))
-            .then(chars => chars.join(''))
+        .then((results) =>
+          results.map((result) => {
+            let char = "";
+            switch (result) {
+              case "success":
+                char = "✅";
+                break;
+              case "canceled":
+                char = "?";
+                break;
+              case "failure":
+                char = "❌";
+                break;
+              default:
+                char = result;
+                break;
+            }
+            return char;
+          })
+        )
+        .then((chars) => chars.join(""))
+    );
+  }
+
+  async refreshData() {
+    if (this.hasAttribute("running")) {
+      console.log("Already running", this.prj);
+      return;
     }
+    this.setAttribute("running", "running");
 
-    async refreshData() {
-        if (this.hasAttribute('running')) {
-            console.log("Already running", this.prj);
-            return ;
-        }
-        this.setAttribute('running', 'running');
+    this.removeAttribute("warning");
+    this.warningLevel = 0;
+    this.lightWarning(0);
 
-        this.removeAttribute('warning');
-        this.warningLevel = 0;
-        this.lightWarning(0);
+    const npm = this.getAttribute("npm");
+    const versionUrl = this.getAttribute("version-url");
+    const ts = new Date().getTime();
 
-        const npm = this.getAttribute('npm');
-        const versionUrl = this.getAttribute('version-url');
-        const ts = new Date().getTime();
+    this.el.githubBadge.innerHTML = `<a class="card-link" href='https://github.com/${this.owner}/${this.prj}'>${this.prj}</a>`;
+    this.el.version.innerHTML = "";
+    this.el.actions.innerHTML = "";
+    this.el.pr.innerHTML = "";
+    this.el.branches.innerHTML = "";
+    this.el.codespaces.innerHTML = "";
 
-        this.el.githubBadge.innerHTML = `<a class="card-link" href='https://github.com/${this.owner}/${this.prj}?${ts}'>${this.prj}</a>`
-        this.el.version.innerHTML = "";
-        this.el.actions.innerHTML = '';
-        this.el.pr.innerHTML = '';
-        this.el.branches.innerHTML = '';
-        this.el.codespaces.innerHTML = '';
+    this.workflows = {};
+    return Promise.all([
+      (this.getAttribute("workflows") ?? "test")
+        .split(",")
+        .forEach(async (workflow) => {
+          this.el.actions.insertAdjacentHTML(
+            "beforeend",
+            `<a id=${workflow} href='https://github.com/${this.owner}/${this.prj}/actions/workflows/${workflow}.yml'>
+                <img 
+                    src='https://github.com/${this.owner}/${this.prj}/actions/workflows/${workflow}.yml/badge.svg?branch=main&ts=${ts}' 
+                    onerror="this.style.display=none"
+                >
+            </a>`
+          );
+          await this.getWorkflowStatuses(
+            3,
+            "main",
+            `.github/workflows/${workflow}.yml`
+          );
+        }),
 
-        this.workflows = {};
-        return Promise.all([
-            (this.getAttribute('workflows') ?? 'test')
-                .split(',')
-                .forEach(async workflow => {
-                    const src = `https://github.com/${this.owner}/${this.prj}/actions/workflows/${workflow}.yml/badge.svg?branch=main`;
-                    this.el.actions.insertAdjacentHTML('beforeend',
-                        `<a id=${workflow} href='https://github.com/${this.owner}/${this.prj}/actions/workflows/${workflow}.yml'>
-                               <img src='${src}' onerror="this.style.display='none'">
-                            </a>`);
-                    await this.getWorkflowStatuses(3, 'main', `.github/workflows/${workflow}.yml`);
-                }),
+      octokit.pulls
+        .list({
+          owner: this.owner,
+          repo: this.prj,
+        })
+        .then((result) => result.data)
+        .then((data) => {
+          data.map(async (pr) => {
+            // console.log(pr);
 
-            octokit.pulls.list({
-                owner: this.owner,
-                repo: this.prj
-            })
-                .then(result => result.data)
-                .then(data => {
-                    data.map(async pr => {
-                        // console.log(pr);
-
-                        this.el.pr.insertAdjacentHTML('beforeend', `
+            this.el.pr.insertAdjacentHTML(
+              "beforeend",
+              `
                                 <div branch='${pr.branch}'>
-                                    <a href='${pr.html_url ?? ''}'>PR: ${pr.user?.login ?? ''} - ${pr.title ?? ''}</a>
-                                    ${await this.getWorkflowStatuses(1, pr.head.ref)}
+                                    <a href='${pr.html_url ?? ""}'>PR: ${
+                pr.user?.login ?? ""
+              } - ${pr.title ?? ""}</a>
+                                    ${await this.getWorkflowStatuses(
+                                      1,
+                                      pr.head.ref
+                                    )}
                                 </div>
-                    `);
-                    })
-                    if (data.length > 0) {
-                        this.lightWarning(1, 'pr');
-                    }
-                    return data;
-                })
-                .then(data => data.map(pr => pr.head.ref))
-                .then(branchesInPr =>
-                    octokit.request('GET /repos/{owner}/{repo}/branches', {
-                        owner: this.owner,
-                        repo: this.prj
-                    })
-                        .then(result => result.data)
-                        // .then(data => Array.isArray(data) ? data : [])
-                        .then(data => data.map(br => br.name))
-                        .then(branches => {
-                            if (branches.includes('gh-pages')) {
-                                this.el.pages.removeAttribute('hidden');
-                            } else {
-                                this.el.pages.setAttribute('hidden', 'hidden');
-                            }
-                            return branches;
-                        })
-                        .then(branches => branches.filter(br => !branchesInPr.includes(br) && br != "main" && br != 'gh-pages'))
-                        .then(branches => branches.map(br => {
-                            // console.log(branches);
-                            this.el.branches.innerHTML += `<div>${br}</div>`
-                        }))
-                )
-                .catch(() => true), // TODO: not clean
+                    `
+            );
+          });
+          if (data.length > 0) {
+            this.lightWarning(1, "pr");
+          }
+          return data;
+        })
+        .then((data) => data.map((pr) => pr.head.ref))
+        .then((branchesInPr) =>
+          octokit
+            .request("GET /repos/{owner}/{repo}/branches", {
+              owner: this.owner,
+              repo: this.prj,
+            })
+            .then((result) => result.data)
+            // .then(data => Array.isArray(data) ? data : [])
+            .then((data) => data.map((br) => br.name))
+            .then((branches) => {
+              if (branches.includes("gh-pages")) {
+                this.el.pages.removeAttribute("hidden");
+              } else {
+                this.el.pages.setAttribute("hidden", "hidden");
+              }
+              return branches;
+            })
+            .then((branches) =>
+              branches.filter(
+                (br) =>
+                  !branchesInPr.includes(br) && br != "main" && br != "gh-pages"
+              )
+            )
+            .then((branches) =>
+              branches.map((br) => {
+                // console.log(branches);
+                this.el.branches.innerHTML += `<div>${br}</div>`;
+              })
+            )
+        )
+        .catch(() => true), // TODO: not clean
 
-            // octokit.request("GET /repos/{owner}/{repo}/codespaces", {
-            //     owner: this.owner,
-            //     repo: this.prj
-            // })
-            //     .then(result => result.data)
-            //     .then(data => {
-            //         data.codespaces.map(cd => this.el.codespaces.insertAdjacentHTML('beforeend', `<a href="${cd.web_url}" class="btn btn-success">${cd.pulls_url ?? 'main'}</a>`));
-            //         //     if (data.codespaces.length <= 0) {
-            //         //         // Codespaces:
-            //         //         //   None is found, we propose to create one:
-            //         //         //
-            //         //         //   see https://docs.github.com/en/rest/codespaces/codespaces#create-a-codespace-for-the-authenticated-user
-            //         //         //
-            //         //         this.el.codespaces.insertAdjacentHTML('beforeend', `
-            //         //     <a class="btn btn-warning">New!</a>
-            //         // `);
-            //         //     this.el.codespaces.querySelector('a').addEventListener('click', () => {
-            //         //         octokit.request('POST /user/codespaces', {
-            //         //             repository_id: 1,
-            //         //             ref: 'main',
-            //         //             location: 'WestUs2'
-            //         //         }).then(data => {
-            //         //             console.log("Created here: ", data.web_url);
-            //         //         })
+      // octokit.request("GET /repos/{owner}/{repo}/codespaces", {
+      //     owner: this.owner,
+      //     repo: this.prj
+      // })
+      //     .then(result => result.data)
+      //     .then(data => {
+      //         data.codespaces.map(cd => this.el.codespaces.insertAdjacentHTML('beforeend', `<a href="${cd.web_url}" class="btn btn-success">${cd.pulls_url ?? 'main'}</a>`));
+      //         //     if (data.codespaces.length <= 0) {
+      //         //         // Codespaces:
+      //         //         //   None is found, we propose to create one:
+      //         //         //
+      //         //         //   see https://docs.github.com/en/rest/codespaces/codespaces#create-a-codespace-for-the-authenticated-user
+      //         //         //
+      //         //         this.el.codespaces.insertAdjacentHTML('beforeend', `
+      //         //     <a class="btn btn-warning">New!</a>
+      //         // `);
+      //         //     this.el.codespaces.querySelector('a').addEventListener('click', () => {
+      //         //         octokit.request('POST /user/codespaces', {
+      //         //             repository_id: 1,
+      //         //             ref: 'main',
+      //         //             location: 'WestUs2'
+      //         //         }).then(data => {
+      //         //             console.log("Created here: ", data.web_url);
+      //         //         })
 
-            //         //     })
-            //         // }
-            //     })
-            //     .catch(() => true), // TODO: not clean
+      //         //     })
+      //         // }
+      //     })
+      //     .catch(() => true), // TODO: not clean
 
-            Promise.resolve()
-                .then(() => {
-                    if (npm) {
-                        return fetch(`https://registry.npmjs.org/${npm}`)
-                            .then(response => response.json())
-                            .then(json => this.el.version.innerHTML = `<a class="btn btn-outline-info" href="https://www.npmjs.com/package/${npm}">version ${json["dist-tags"].latest}</a>`)
-                    } else if (versionUrl) {
-                        return fetch(versionUrl)
-                            .then(response => response.text())
-                            .then(text => this.el.version.innerHTML = `<span class="btn btn-outline-info" >version ${text}</span>`)
-                    }
-                })
-                .catch(() => true), // TODO: not clean
-        ]
-        ).then(data => {
-            this.removeAttribute('running');
-            return data;
-        });
-    }
+      Promise.resolve()
+        .then(() => {
+          if (npm) {
+            return fetch(`https://registry.npmjs.org/${npm}`)
+              .then((response) => response.json())
+              .then(
+                (json) =>
+                  (this.el.version.innerHTML = `<a class="btn btn-outline-info" href="https://www.npmjs.com/package/${npm}">version ${json["dist-tags"].latest}</a>`)
+              );
+          } else if (versionUrl) {
+            return fetch(versionUrl)
+              .then((response) => response.text())
+              .then(
+                (text) =>
+                  (this.el.version.innerHTML = `<span class="btn btn-outline-info" >version ${text}</span>`)
+              );
+          }
+        })
+        .catch(() => true), // TODO: not clean
+    ]).then((data) => {
+      this.removeAttribute("running");
+      return data;
+    });
+  }
 }
 
 customElements.define(XRepository.tag, XRepository);
