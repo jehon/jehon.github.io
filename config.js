75226e981ed4a768719ea4b85b54a8d069666734
Jehon <1582670+jehon@users.noreply.github.com>
2023-05-25 14:50:14 +0000

    refactor: use infrastructure project

--- config.js
+++ config.js
@@ -1,31 +1,35 @@
-
 const config = {};
 
 try {
-    Object.assign(config, await fetch("devstack/dev-config.json")
-        .then(response => response.json(), () => null)
-        );
-} catch(_e) {
-    // ok
+  Object.assign(
+    config,
+    await fetch("infrastructure/dev-config.json").then(
+      (response) => response.json(),
+      () => null
+    )
+  );
+} catch (_e) {
+  // ok
 }
 
 // export default config;
 
 // Thanks to https://stackoverflow.com/a/56253298/1954789
 function flattenObj(obj, parent, res = {}) {
-    for (let key in obj) {
-        let propName = parent ? parent + '.' + key : key;
-        if (typeof obj[key] == 'object') {
-            flattenObj(obj[key], propName, res);
-        } else {
-            res[propName] = obj[key];
-        }
+  for (let key in obj) {
+    let propName = parent ? parent + "." + key : key;
+    if (typeof obj[key] == "object") {
+      flattenObj(obj[key], propName, res);
+    } else {
+      res[propName] = obj[key];
     }
-    return res;
+  }
+  return res;
 }
 const flattenConfig = flattenObj(config);
 
-export const inject = (string) => string.replace(
+export const inject = (string) =>
+  string.replace(
     /\{\{([0-9a-zA-Z_.]+)\}\}/g,
     (full, name) => flattenConfig[name] ?? ""
-)
+  );
