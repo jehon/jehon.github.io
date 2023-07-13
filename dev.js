34db5be142d32e1a36ca0d3b41ffa8d0d9a30f2b
Jean Honlet <jehon@users.noreply.github.com>
2023-07-13 10:17:06 +0200

    feat: do not initial refresh

--- dev.js
+++ dev.js
@@ -1,32 +1,36 @@
-
 import "./x-img.js";
 import "./x-repository.js";
 
-const timestampEl = document.querySelector('#timestamp');
-const timeUTCEl = document.querySelector('#time');
+const timestampEl = document.querySelector("#timestamp");
+const timeUTCEl = document.querySelector("#time");
 
 setInterval(() => {
-    timeUTCEl.innerHTML = (new Date()).toISOString().substring(11, 19) + " UTC";
+  timeUTCEl.innerHTML = new Date().toISOString().substring(11, 19) + " UTC";
 }, 1000);
 
 let refreshCron = 0;
 export async function refresh() {
-    if (refreshCron) {
-        clearTimeout(refreshCron)
-    }
-    refreshCron = setTimeout(() => refresh(), 5 * 60 * 1000);
+  if (refreshCron) {
+    clearTimeout(refreshCron);
+  }
+  refreshCron = setTimeout(() => refresh(), 5 * 60 * 1000);
 
-    timestampEl.innerHTML = (new Date()).toTimeString().split(' ')[0];
+  timestampEl.innerHTML = new Date().toTimeString().split(" ")[0];
 
-    return Promise.all(Array.from(document.querySelectorAll('x-repository')).map(el => el.refreshData()));
+  return Promise.all(
+    Array.from(document.querySelectorAll("x-repository")).map((el) =>
+      el.refreshData()
+    )
+  );
 }
 
 document.refresh = refresh;
-refresh();
 
-timestampEl.addEventListener('click', () => refresh());
+timestampEl.addEventListener("click", () => refresh());
 
-document.querySelectorAll('[copy-to-clipboard]').forEach(e => e.addEventListener("click", e => {
+document.querySelectorAll("[copy-to-clipboard]").forEach((e) =>
+  e.addEventListener("click", (e) => {
     const txt = e.target.innerHTML;
     navigator.clipboard.writeText(txt);
-}));
+  })
+);
