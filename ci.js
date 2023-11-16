2fb95330ec0e885e7a04b1f42fd201f629ccb42c
Jean Honlet <jean.honlet@focus.be>
2023-11-16 15:48:31 +0000

    feat: add ci.html

---
+++ ci.js
@@ -0,0 +1,25 @@
+class CI extends HTMLElement {
+  static get tag() {
+    return "x-ci";
+  }
+
+  connectedCallback() {
+    const project = this.getAttribute("project");
+    const branch = this.getAttribute("branch");
+
+    this.style.gridRow = project.replaceAll("/", "_");
+    this.style.gridColumn = branch;
+
+    this.innerHTML = `
+        <a
+            href="https://gitlab.redange.fr-team.lu/${project}/-/network/${branch}?ref_type=heads"
+            >
+            <img
+                alt="pipeline status"
+                src="https://gitlab.redange.fr-team.lu/${project}/badges/${branch}/pipeline.svg?ignore_skipped=true"
+                />
+        </a>`;
+  }
+}
+
+customElements.define(CI.tag, CI);
